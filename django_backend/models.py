316b6858150f2ee7618807a95574667dace403f2
alanifotis <alanifotis@yahoo.com>
2025-05-09 09:31:31 +0200

    things are happening live...

--- django_backend/models.py
+++ django_backend/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=False,)
+    short_description = models.CharField(max_length=255, null=False,)
+    product_description = models.CharField(max_length=512, null=True,)
+    stock = models.IntegerField(null=False)
+    price = models.DecimalField(null=False)
