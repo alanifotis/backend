4b4d83b329a107dcf7eed1bf030ecc22af92aadb
alanifotis <alanifotis@yahoo.com>
2025-05-09 12:47:56 +0200

    things are working...better??

--- django_backend/main/urls.py
+++ django_backend/main/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
 from main import views
-
 urlpatterns = [
-    path('products/<int:pk>/', views.product_detail),
+    path('products/<int:pk>', views.product_detail),
     path('products', views.product_list),
     path('products/create', views.create_product),
 ]
